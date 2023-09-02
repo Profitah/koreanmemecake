f74621f09196e0ecd5bec8953c4f3f5daacadd49
profitah <qwer120542@gmail.com>
2023-09-02 18:01:54 +0900

    안 사랑해 ㅅㅂ

--- circleslide.js
+++ circleslide.js
@@ -1,14 +1,35 @@
+
 $(".slide").children().css("width", "8em");
+
 
 (function() {
     var method;
     var noop = function() {};
     var methods = [
-      'Rei', '', 'Liz', 'Gaeul', 'Leeseo', 'An Yujin', 'ang Wonyoung',
+      'Rei', '', 'Liz', 'Gaeul', 'Leeseo', 'An Yujin', 'Jang Wonyoung',
       
     ];
     var length = methods.length;
     var console = (window.console = window.console || {});
-
-
-})
+    while (length--) {
+        method = methods[length];
+    
+        if (!console[method]) {
+          console[method] = noop;
+        }
+      }
+$('.slide li').hover(
+    function() {
+        $(this).css({
+            'background-color': 'black',
+            'color': 'white'
+        });
+    },
+    function() {
+        $(this).css({
+            'background-color': '',
+            'color': ''
+        });
+    }
+);
+    }());
