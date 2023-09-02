cb1a92f9deb154cdd37eeb8865a67cb6fbaef32e
profitah <qwer120542@gmail.com>
2023-09-02 14:30:18 +0900

    css fix1

---
+++ circleslide.js
@@ -0,0 +1,14 @@
+$(".slide").children().css("width", "8em");
+
+(function() {
+    var method;
+    var noop = function() {};
+    var methods = [
+      'Rei', '', 'Liz', 'Gaeul', 'Leeseo', 'An Yujin', 'ang Wonyoung',
+      
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+
+})
